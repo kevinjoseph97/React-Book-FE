6300184a5fde44ef6adfb94eda46384dab44316a
kevinjoseph97 <kevinjoseph97@gmail.com>
2021-07-12 18:08:44 -0400

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello There </h1>
+            </div>
+        )
+    }
+}
+
+
+export default Navbar
